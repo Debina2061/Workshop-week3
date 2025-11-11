f66c9f02dbf09026aac49437bf47b43f01db4f5a
Debina <debinabaraili94@gmail.com>
2025-11-11 11:25:02 +0545

    Task 1

---
+++ Task 1/Operator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Operators
+{
+    public void Add(int a, int b)
+    {
+        Console.WriteLine("Sum: " + (a + b));
+    }
+
+    public void Subtract(int a, int b)
+    {
+        Console.WriteLine("Difference: " + (a - b));
+    }
+
+    public void Multiply(int a, int b)
+    {
+        Console.WriteLine("Product: " + (a * b));
+    }
+
+    public void Divide(int a, int b)
+    {
+        if (b != 0)
+            Console.WriteLine("Division: " + ((double)a / b));
+        else
+            Console.WriteLine("Cannot divide by zero!");
+    }
+
+    public void OddEvenFinder(int number)
+    {
+        string result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+        Console.WriteLine(result);
+    }
+}
