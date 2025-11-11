fc8a357d4dfc09616f4e9d6bf2fb70e4e18ac344
Debina <debinabaraili94@gmail.com>
2025-11-11 11:48:55 +0545

    Task 3

---
+++ Task 3/Program.cs
@@ -0,0 +1,34 @@
+﻿
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Ask the user for their age
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        // Using if-else-if
+        if (age < 13)
+        {
+            Console.WriteLine("Child");
+        }
+        else if (age >= 13 && age <= 19)
+        {
+            Console.WriteLine("Teenager");
+        }
+        else
+        {
+            Console.WriteLine("Adult");
+        }
+
+        // Using switch expression
+        string category = age switch
+        {
+            < 13 => "Child",
+            >= 13 and <= 19 => "Teenager",
+            _ => "Adult"
+        };
+
+        Console.WriteLine($"(Using switch) You are a {category}.");
+    }
+}
